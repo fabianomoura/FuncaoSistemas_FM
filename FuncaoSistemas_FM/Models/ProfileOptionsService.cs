a4da105c16a31ef2b47f63cb47d407c1e5a7de3a
Fabiano Moura <fabiano@fabianomoura.com.br>
2020-12-10 11:08:43 -0300

    Refatoração da tela de incluir e alterar o Cliente

---
+++ FuncaoSistemas_FM/Models/ProfileOptionsService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FuncaoSistemas_FM.Models
+{
+    public static class ProfileOptionsService
+    {
+        public static ICollection<SelectListItem> ListaEstados()
+        {
+            List<SelectListItem> items = new List<SelectListItem>();            
+            items.Insert(0, (new SelectListItem() { Value = "", Text = "Selecione" }));
+            items.Insert(1, (new SelectListItem() { Value = "AC", Text = "Acre" }));
+            items.Insert(2, (new SelectListItem() { Value = "AL", Text = "Alagoas" }));
+            items.Insert(3, (new SelectListItem() { Value = "AP", Text = "Amapá" }));
+            items.Insert(4, (new SelectListItem() { Value = "AM", Text = "Amazonas" }));
+            items.Insert(5, (new SelectListItem() { Value = "BA", Text = "Bahia" }));
+            items.Insert(6, (new SelectListItem() { Value = "CE", Text = "Ceará" }));
+            items.Insert(7, (new SelectListItem() { Value = "DF", Text = "Distrito Federal" }));
+            items.Insert(8, (new SelectListItem() { Value = "ES", Text = "Espírito Santo" }));
+            items.Insert(9, (new SelectListItem() { Value = "GO", Text = "Goiás" }));
+            items.Insert(10, (new SelectListItem() { Value = "MA", Text = "Maranhão" }));
+            items.Insert(11, (new SelectListItem() { Value = "MT", Text = "Mato Grosso" }));
+            items.Insert(12, (new SelectListItem() { Value = "MS", Text = "Mato Grosso do Sul" }));
+            items.Insert(13, (new SelectListItem() { Value = "MG", Text = "Minas Gerais" }));
+            items.Insert(14, (new SelectListItem() { Value = "PA", Text = "Pará" }));
+            items.Insert(15, (new SelectListItem() { Value = "PB", Text = "Paraíba" }));
+            items.Insert(16, (new SelectListItem() { Value = "PR", Text = "Paraná" }));
+            items.Insert(17, (new SelectListItem() { Value = "PE", Text = "Pernambuco" }));
+            items.Insert(18, (new SelectListItem() { Value = "PI", Text = "Piauí" }));
+            items.Insert(19, (new SelectListItem() { Value = "RJ", Text = "Rio de Janeiro" }));
+            items.Insert(20, (new SelectListItem() { Value = "RN", Text = "Rio Grande do Norte" }));
+            items.Insert(21, (new SelectListItem() { Value = "RS", Text = "Rio Grande do Sul" }));
+            items.Insert(22, (new SelectListItem() { Value = "RO", Text = "Rondônia" }));
+            items.Insert(23, (new SelectListItem() { Value = "RR", Text = "Roraima" }));
+            items.Insert(24, (new SelectListItem() { Value = "SC", Text = "Santa Catarina" }));
+            items.Insert(25, (new SelectListItem() { Value = "SP", Text = "São Paulo" }));
+            items.Insert(26, (new SelectListItem() { Value = "SE", Text = "Sergipe" }));
+            items.Insert(27, (new SelectListItem() { Value = "TO", Text = "Tocantins" }));
+
+            return items;
+        }
+    }
+}
---
+++ FuncaoSistemas_FM/Models/Util.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FuncaoSistemas_FM.Models
+{
+    public static class Util
+    {
+        public static bool ValidaCPF(string Cpf)
+        {
+            string valor;
+            if (Cpf.Length > 11)
+                valor = FormataCPF(Cpf);
+            else
+                valor = Cpf;
+            bool igual = true;
+
+            for (int i = 1; i < 11 && igual; i++)
+                if (valor[i] != valor[0])
+                    igual = false;
+
+            if (igual || valor == "12345678909")
+                return false;
+
+            int[] numeros = new int[11];
+
+            for (int i = 0; i < 11; i++)
+                numeros[i] = int.Parse(valor[i].ToString());
+
+            int soma = 0;
+            for (int i = 0; i < 9; i++)
+                soma += (10 - i) * numeros[i];
+
+            int resultado = soma % 11;
+
+            if (resultado == 1 || resultado == 0)
+            {
+                if (numeros[9] != 0)
+                    return false;
+            }
+            else if (numeros[9] != 11 - resultado)
+                return false;
+
+            soma = 0;
+            for (int i = 0; i < 10; i++)
+                soma += (11 - i) * numeros[i];
+
+            resultado = soma % 11;
+
+            if (resultado == 1 || resultado == 0)
+            {
+                if (numeros[10] != 0)
+                    return false;
+            }
+            else
+              if (numeros[10] != 11 - resultado)
+                return false;
+
+            return true;
+        }
+
+        public static string FormataCPF(string Cpf)
+        {
+            string valor = Cpf;
+            valor = valor.Replace(".", "");
+            valor = valor.Replace("-", "");
+            return valor;
+        }
+    }
+}
